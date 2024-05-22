dcd33baa8228932fee747405b3d6f7930f1bf13e
Ravshanbek Tursunbaev <rtursunbaev9822@gmail.com>
2024-05-22 12:57:13 +0500

    intial commit

---
+++ lib/fooderlich_theme.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FooderlichTheme {
+
+// 1
+  static TextTheme lightTextTheme = TextTheme(
+    bodyMedium: GoogleFonts.openSans(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+    ),
+    headlineLarge: GoogleFonts.openSans(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    headlineMedium: GoogleFonts.openSans(
+      fontSize: 21.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+    ),
+    headlineSmall: GoogleFonts.openSans(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+    titleMedium: GoogleFonts.openSans(
+      fontSize: 20.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+  );
+
+// 2
+  static TextTheme darkTextTheme = TextTheme(
+    bodyMedium: GoogleFonts.openSans(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.white,
+    ),
+    headlineLarge: GoogleFonts.openSans(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ),
+    headlineMedium: GoogleFonts.openSans(
+      fontSize: 21.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.white,
+    ),
+    headlineSmall: GoogleFonts.openSans(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    ),
+    titleMedium: GoogleFonts.openSans(
+      fontSize: 20.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    ),
+  );
+
+// 3
+  static ThemeData light() {
+    return ThemeData(
+        brightness: Brightness.light,
+        checkboxTheme:
+            CheckboxThemeData(fillColor: WidgetStateColor.resolveWith((states) {
+          return Colors.black;
+        })),
+        appBarTheme: const AppBarTheme(
+            foregroundColor: Colors.black, backgroundColor: Colors.white),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+            foregroundColor: Colors.white, backgroundColor: Colors.black),
+        bottomNavigationBarTheme:
+            const BottomNavigationBarThemeData(selectedItemColor: Colors.green),
+        textTheme: lightTextTheme);
+  }
+
+// 4
+  static ThemeData dark() {
+    return ThemeData(
+
+      brightness: Brightness.dark,
+
+      appBarTheme: AppBarTheme(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.grey[900],
+      ),
+
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.green,
+      ),
+
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        selectedItemColor: Colors.green,
+      ),
+
+      textTheme: darkTextTheme,
+
+    );
+  }
+
+  //final theme = FooderlichTheme.dark().textTheme.displayMedium;
+}
